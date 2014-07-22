6d916c28fccbad4ca9de8dcfb9fe0ee05fcebc36
Lorenz Bausch <gitlab@lorenzbausch.de>
2014-07-23 01:48:21 +0200

    fixed typos

--- code/bootstrap/database.php
+++ code/bootstrap/database.php
@@ -7,7 +7,7 @@ Config::set('database.connections.sqlgrey', $db_settings['databases']['sqlgrey']
 
 $db_array_keys = array_keys($db_settings['databases']['application']);
 
-// check if the applications should use the same database as SQLgrey
+// check if the application should use the same database as SQLgrey
 if (count($db_array_keys) === 1 && @$db_array_keys[0] == 'prefix') {
     Config::set('database.connections.application', $db_settings['databases']['sqlgrey']);
     Config::set('database.connections.application.prefix', $db_settings['databases']['application']['prefix']);
