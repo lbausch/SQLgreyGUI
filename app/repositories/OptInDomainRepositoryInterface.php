b55e450e4586d9f900f71e5516b98b8cca8a64c8
Lorenz Bausch <gitlab@lorenzbausch.de>
2014-07-24 02:24:40 +0200

    deleted unnecessary files

--- app/repositories/OptInDomainRepositoryInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Bausch\Repositories;
-
-interface OptInDomainRepositoryInterface extends OptDomainRepositoryInterface {
-    
-}
--- app/repositories/OptInEmailRepositoryInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Bausch\Repositories;
-
-interface OptInEmailRepositoryInterface extends OptEmailRepositoryInterface {
-    
-}
