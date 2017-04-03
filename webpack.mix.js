40604888b398e26255608ee370d0ca0028a022b4
Lorenz Bausch <info@lorenzbausch.de>
2017-04-03 14:36:22 +0200

    Enable extraction

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const {mix} = require('laravel-mix');
+const {mix} = require('laravel-mix')
 
 /*
  |--------------------------------------------------------------------------
@@ -11,33 +11,33 @@ const {mix} = require('laravel-mix');
  |
  */
 
-mix.disableNotifications();
+mix.disableNotifications()
 
 mix.browserSync({
-    open: false,
-    proxy: 'sqlgreygui.local',
-    files: [
-        'app/**/*.php',
-        'resources/views/**/*.php',
-        'resources/views/**/*.twig',
-        'public/assets/js/**/*.js',
-        'public/assets/css/**/*.css'
-    ]
-});
+  open: false,
+  proxy: 'sqlgreygui.local',
+  files: [
+    'app/**/*.php',
+    'resources/views/**/*.php',
+    'resources/views/**/*.twig',
+    'public/assets/js/**/*.js',
+    'public/assets/css/**/*.css'
+  ]
+})
 
-mix.setPublicPath('public/assets');
-mix.setResourceRoot('/assets/');
+mix.setPublicPath('public/assets')
+mix.setResourceRoot('/assets/')
 
 mix.sass('resources/assets/sass/style.scss', 'css')
-    .js('resources/assets/js/app.js', 'js')
-    .js('resources/assets/js/public.js', 'js');
-/*.extract([
- 'axios',
- 'lodash',
- 'vue',
- 'vue-strap',
- ]);*/
+  .js('resources/assets/js/app.js', 'js')
+  .js('resources/assets/js/public.js', 'js')
+  .extract([
+    'axios',
+    'lodash',
+    'vue',
+    'vue-strap'
+  ])
 
 if (mix.config.inProduction) {
-    mix.version();
+  mix.version()
 }
