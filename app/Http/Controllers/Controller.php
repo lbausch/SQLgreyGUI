50ab8855e0aa83a4641e41b430c369ebb3adf08c
Lorenz Bausch <info@lorenzbausch.de>
2017-04-03 14:41:07 +0200

    Remove dead code

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,27 +10,4 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    /**
-     * Parse provided entries.
-     *
-     * @return array
-     */
-    /*protected function parseEntries($input_identifier, $repository)
-    {
-        $items_tmp = request()->input($input_identifier, []);
-
-        $items = [];
-
-        $repository = app($repository);
-
-        foreach ($items_tmp as $key => $val) {
-            $model = json_decode(base64_decode($val, $strict = true), $assoc = true);
-
-            $items[] = $repository->instance($model);
-        }
-
-        return $items;
-    }*/
-
 }
