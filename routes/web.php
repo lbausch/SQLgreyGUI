541e9faabb5cf72b78fe10395f25b14a83de0ea9
Lorenz Bausch <info@lorenzbausch.de>
2017-05-04 17:54:38 +0200

    Update to latest Laravel codebase

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 // Authentication Routes...
 $router->get('login', 'Auth\LoginController@showLoginForm')->name('login');
 $router->post('login', 'Auth\LoginController@login');
